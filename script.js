00846764c5a4eaca19999d1147b3d84130e4ac6d
romankh3 <roman.beskrovnyy@gmail.com>
2018-06-21 15:40:52 +0300

    Section 1, Lecture 2.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.write('Hello World');
+// comments using like in Java:
+// first
+/*
+    second.
+*/
+
+document.write('Hello ');
+document.write('world');
+// the output will be 'Hello World'.
+// it means that it's like System.out.print(); in Java.
