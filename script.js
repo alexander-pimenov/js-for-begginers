ae1df1b186e1505f94c7b259f1b585adfa002fad
romankh3 <roman.beskrovnyy@gmail.com>
2018-06-21 16:11:13 +0300

    Section 1, Lecture 5

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-var i = -25;
-document.write(i);
-// data types:
-// String
-// numbers
-// boolean
-var b = true;
-var n = null;
-var theSame = b;
-i = "Mother said go \"wash dishes\"";
-document.write('   ' + i);
-if(b == theSame) {
-  document.write('the same');
-}
+var i = "my Mom ";
+var y = "likes to cook.";
+document.write(i+y);
+
+// or
+
+document.write('      ');
+var y2 = 232;
+var i2 = 234;
+
+var result = y2 + i2;
+document.write(y2+i2+' the same as '+result);
