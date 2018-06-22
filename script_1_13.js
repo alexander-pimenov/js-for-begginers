1bdd7c3ac1b324492d935d8c90613b0fc4666c1d
romankh3 <roman.beskrovnyy@gmail.com>
2018-06-22 11:30:18 +0300

    Section 1, Lecture 13. Math.

---
+++ script_1_13.js
@@ -0,0 +1,33 @@
+//sum
+var apple = 4 + 6;
+var br = "<br>";
+
+document.write(apple + br);
+
+//multiply.
+var apple2 = apple * 3;
+
+document.write(apple2 + br);
+
+// devide
+var apple3 = apple2 / 2;
+
+document.write(apple3+br);
+
+// least.
+var a4 = apple3 % 3;
+
+document.write(a4 + br);
+
+//var + 1;
+a4++;
+document.write(a4 + br);
+
+//var - 1;
+a4--;
+document.write(a4 + br);
+
+//equal apple = apple + 5;
+apple += 5;
+
+document.write(apple);
