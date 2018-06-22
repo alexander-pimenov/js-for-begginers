65ecb796724b09c992dfd6ca22b7bc746a6d726c
romankh3 <roman.beskrovnyy@gmail.com>
2018-06-22 10:28:04 +0300

    Section 1, Lecture 11. Local and Global Variables.

---
+++ script_1_12.js
@@ -0,0 +1,11 @@
+// global variable.
+var fruit = "apple";
+
+function word() {
+  // local variable.
+  var fr = "yo bro";
+  alert(fr + "! I'm an " + fruit + "!");
+}
+
+// it shouldn't show anything.
+document.write(fr);
