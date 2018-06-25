ce49acca9bd29cd57087a5f4c36caa1dc511c8b4
romankh3 <roman.beskrovnyy@gmail.com>
2018-06-25 13:09:47 +0300

    Section 1, Lecture 18.

---
+++ script_1_18.js
@@ -0,0 +1,10 @@
+var firstName = "Jay";
+var lastName = "Cutler";
+
+if((firstName=="Jay") && (lastName=="Cutler")) {
+  document.write("true");
+} else {
+  document.write("false");
+}
+
+// operators the same as for Java: && - end, || - or.
