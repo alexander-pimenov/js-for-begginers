3d5732a9d337aef8d6d92d77005f9b01c7fd0183
romankh3 <roman.beskrovnyy@gmail.com>
2018-06-26 10:44:28 +0300

    section 1, Lecture 21. While loop.

---
+++ script_1_21.js
@@ -0,0 +1,11 @@
+for(i = 0; i < 5; i++) {
+  document.write("You're the best!<br>");
+}
+
+var y = 0;
+//almost the same as for Java.
+document.write("<br>");
+while(y < 5) {
+  document.write("You're the best!<br>");
+  y++;
+}
