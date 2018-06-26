20d07f26f39c751b2e6f8ccaf40be2ad4fec6a74
romankh3 <roman.beskrovnyy@gmail.com>
2018-06-26 10:35:39 +0300

    Section 1, Lecture 20. For Loop.

---
+++ script_1_20.js
@@ -0,0 +1,12 @@
+function say(i) {
+  if(i % 2) {
+    document.write("I know how to use for in JS! :D. i % 2 = 0 <br>");
+  } else {
+    document.write("I know how to use for in JS! :D. i % 2 = 1 <br>");
+  }
+}
+
+// as Like as for Java.
+for(i = 0; i < 5; i++) {
+  say(i);
+}
