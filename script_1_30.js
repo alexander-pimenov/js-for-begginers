3a11ca87c98f2c8b3851a716787f192719c1bb28
romankh3 <roman.beskrovnyy@gmail.com>
2018-06-29 15:02:51 +0300

    Section 1, Lecture 30. Multiple choises to create an array.

---
+++ script_1_30.js
@@ -0,0 +1,14 @@
+// Multiples choises to create an array.
+
+var people = new Array(3);
+
+people[0] = "one"
+people[1] = "two"
+people[2] = "three"
+people[3] = "four"
+people[4] = "five"
+people[5] = "six"
+
+for(i = 0; i < people.length; i ++) {
+  document.write(people[i] + "<br>");
+}
