abfc33dc2f1a0fc7aa1dc5486e321200637360bd
romankh3 <roman.beskrovnyy@gmail.com>
2018-06-29 14:05:35 +0300

    Section 1, Lecture 27. Another way to create an object.

---
+++ script_1_27.js
@@ -0,0 +1,14 @@
+// another way to create an object.
+Jay = {
+  name: "Jat Cutler",
+  age: 40
+}
+
+Ronnie = {
+  name: "Ronnie Coleman",
+  age: 50
+}
+
+document.write(Jay.name + " " + Jay.age);
+document.write("<br>");
+document.write(Ronnie.name + " " + Ronnie.age);
