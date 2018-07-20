b288a53fb3271e72ba45a5a9d3e82ea997c71cef
romankh3 <roman.beskrovnyy@gmail.com>
2018-07-20 10:36:04 +0300

    Section 1, Lecture 31. Arrrays with properties and methods

---
+++ script_1_31.js
@@ -0,0 +1,14 @@
+// Arrrays with properties and methods
+var guys1 = new Array("Jay", "Ronnie", "Kai");
+var guys2 = new Array("Arnold", "Frank", "Markus");
+
+document.write(guys1.length + "<br>");
+// concat two arrays in one.
+var people = guys1.concat(guys2);
+
+document.write(people[0] + "<br>");
+document.write(people[1] + "<br>");
+document.write(people[2] + "<br>");
+document.write(people[3] + "<br>");
+document.write(people[4] + "<br>");
+document.write(people[5] + "<br>");
