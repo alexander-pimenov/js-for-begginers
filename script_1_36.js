9c0c548516be60fd89e82a8ac91b1744f28d9797
romankh3 <roman.beskrovnyy@gmail.com>
2018-07-24 10:08:58 +0300

    Section 1, Lecture 36: associative arrays.

---
+++ script_1_36.js
@@ -0,0 +1,9 @@
+// Associative Arrays
+var apple = new Array();
+
+apple["fruit"] = "apple";
+apple["car"] = "volvo";
+
+document.write(apple["car"]);
+
+// in total it means that Array can get numbers ot strings as a key.
