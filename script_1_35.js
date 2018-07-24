b365ff62361908c492a19a0543a24c45b523568e
romankh3 <roman.beskrovnyy@gmail.com>
2018-07-24 10:01:51 +0300

    Section 1, Lecture 35: easier method how to output the aaray.

---
+++ script_1_35.js
@@ -0,0 +1,9 @@
+// Easier method how to Output the Whole array.
+
+var stuff = new Array("v", "a", "y", "u", "b", "c");
+
+stuff.sort();
+
+for(i = 0; i < stuff.length; i++) {
+  document.write(stuff[i] + "<br>");
+}
