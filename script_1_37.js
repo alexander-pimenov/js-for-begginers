41aa6fa0e3cfaa5dd069be2206ee312292321815
romankh3 <roman.beskrovnyy@gmail.com>
2018-07-24 10:14:02 +0300

    Section 1, Lecture 37: Math object and math operations.

---
+++ script_1_37.js
@@ -0,0 +1,12 @@
+// Math Objects:
+
+document.write(Math.PI);
+
+var number = prompt("Enter some number");
+var answer = Math.sqrt(number);
+
+alert("Square root of number " + number + " is " + answer);
+
+
+// in total:
+// Math is an object for math operations.
